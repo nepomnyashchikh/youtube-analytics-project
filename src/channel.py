0b80a805b1fb4b6340bbed5f5f9a04d034800eea
nepomnyashchikh <nepomnyashchikh.a@mail.ru>
2023-06-04 16:39:57 +0700

    Модифицируйте конструктор `Channel`, чтобы после инициализации экземпляр имел следующие атрибуты, заполненные реальными данными канала: - id канала - название канала - описание канала - ссылка на канал - количество подписчиков - количество видео - общее количество просмотров

--- src/channel.py
+++ src/channel.py
@@ -2,8 +2,6 @@ import json
 import os
 from googleapiclient.discovery import build
 
-
-
 api_key: str = os.getenv('API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -14,6 +12,37 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = Channel.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = channel['items'][0]['snippet']['thumbnails']["default"]['url']
+        self.subscriberCount = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.viewCount = channel['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        return f'{self.title}, {self.url}'
+
+    def __add__(self, other):
+        return int(self.subscriberCount) + int(other.subscriberCount)
+
+    def __sub__(self, other):
+        return int(self.subscriberCount) - int(other.subscriberCount)
+
+    def __gt__(self, other):
+        return int(self.subscriberCount) > int(other.subscriberCount)
+
+    def __ge__(self, other):
+        return int(self.subscriberCount) >= int(other.subscriberCount)
+
+    def __lt__(self, other):
+        return int(self.subscriberCount) < int(other.subscriberCount)
+
+    def __le__(self, other):
+        return int(self.subscriberCount) <= int(other.subscriberCount)
+
+    def __eq__(self, other):
+        return int(self.subscriberCount) == int(other.subscriberCount)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
