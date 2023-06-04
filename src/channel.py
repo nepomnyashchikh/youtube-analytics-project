0d08839cea6360a5f63ffc6de7b75b20b7791fda
nepomnyashchikh <nepomnyashchikh.a@mail.ru>
2023-06-04 18:20:59 +0700

    Добавьте в класс `Channel` следующие методы: - класс-метод `get_service()`, возвращающий объект для работы с YouTube API - метод `to_json()`, сохраняющий в файл значения атрибутов экземпляра `Channel`

--- src/channel.py
+++ src/channel.py
@@ -7,10 +7,15 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """
+    Класс для ютуб-канала
+    """
+    dict_hw_2 = {}
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """
+        Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
+        """
         self.channel_id = channel_id
         channel = Channel.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = channel['items'][0]['snippet']['title']
@@ -45,7 +50,32 @@ class Channel:
         return int(self.subscriberCount) == int(other.subscriberCount)
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
+        """
+        метод-класса `Channel` выводит в консоль информацию о канале.
+        """
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         info = json.dumps(channel, indent=2, ensure_ascii=False)
         print(info)
+
+    @classmethod
+    def get_service(cls):
+        """
+        класс-метод `get_service()`, возвращающий объект для работы с YouTube API
+        """
+        object_get = build('youtube', 'v3', developerKey=api_key)
+        return object_get
+
+    def to_json(self, file_name):
+        """
+        метод `to_json()`, сохраняющий в файл значения атрибутов экземпляра `Channel`
+        """
+        self.dict_hw_2['id'] = self.__channel_id
+        self.dict_hw_2['title'] = self.title
+        self.dict_hw_2['description'] = self.description
+        self.dict_hw_2['url'] = self.url
+        self.dict_hw_2['subscriberCount'] = self.subscriberCount
+        self.dict_hw_2['video_count'] = self.video_count
+        self.dict_hw_2['viewCount'] = self.viewCount
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            json.dump(self.dict_hw_2, f, indent=2, ensure_ascii=False)
